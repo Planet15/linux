94187fcca0f087a24bf8ed56a23b6332ea4bb446
Leo Song <casong99@gmail.com>
2015-07-13 07:36:38 +0900

    This driver is based on stmmac driver.
    changes since v2:
    - use tab instead of space for macros
    - use HIWORD_UPDATE macro for GMAC_CLK_RX_DL_CFG and GMAC_CLK_TX_DL_CFG
    - remove drive-strength setting in the driver and set it in the pinctrl settings
    - use dev_err instead of pr_err
    - remove clock names's macros, just use the real name of the clock
    - use devm_clk_get() instead of clk_get()
    - remove clk_set_parent(bsp_priv->clk_mac, bsp_priv->clk_mac_pll)
    - remove gpio setting for LDO, just use regulator API
    - remove phy reset using gpio in the glue layer, it has been handled in the stmmac driver
    - remove handling phy interrupt (mii interrupt)
    
    changes since v1:
    - use BIT() to set register
    - combine two remap_write() operations into one for the same register
    - use macros for register value setting
    - remove grf fail check in rk_gmac_setup() and save all the check in set_rgmii_speed()
    - remove .tx_coe=1 in rk_gmac_data

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -45,6 +45,9 @@ static const struct of_device_id stmmac_dt_ids[] = {
 #ifdef CONFIG_DWMAC_SOCFPGA
 	{ .compatible = "altr,socfpga-stmmac", .data = &socfpga_gmac_data },
 #endif
+#ifdef CONFIG_DWMAC_ROCKCHIP 
+	{ .compatible = "rockchip,rk3288-gmac", .data = &rk3288_gmac_data },
+#endif
 	/* SoC specific glue layers should come before generic bindings */
 	{ .compatible = "st,spear600-gmac"},
 	{ .compatible = "snps,dwmac-3.610"},
